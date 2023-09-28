00940ebb1e085e8b717da56e4590a08bb73f9d51
khinoj <kristopher.hinojosa@gmail.com>
2023-09-27 21:45:11 -0400

    router-delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,10 +19,10 @@ router.get('/', async (req, res) => {
 
 // find one category by its `id` value
 // be sure to include its associated Products
-router.get('/:id',  async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const categoriesData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product}],
+      include: [{ model: Product }],
     });
 
     if (!categoriesData) {
@@ -40,7 +40,7 @@ router.get('/:id',  async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const categoriesData = await Category.create(
-       req.body
+      req.body
     );
     res.status(200).json(categoriesData);
   } catch (err) {
@@ -48,12 +48,31 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+
+
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoriesData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoriesData) {
+      res.status(404).json({ message: 'No locatoin with this id!' })
+      return;
+    }
+
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
